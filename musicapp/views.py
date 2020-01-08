2ca3293663102356489d9ac2b3ea8cd7afd5048d
Alec Stephens <astephens91@gmail.com>
2020-01-08 12:33:13 -0500

    Created views + templates
    Created two class based views for album details and user details and made to simple templates to go with them

--- musicapp/views.py
+++ musicapp/views.py
@@ -1,14 +1,39 @@
 from django.shortcuts import render, HttpResponseRedirect, reverse
 from django.views import View
 from musicapp.albums.models import Album
+from musicapp.jamusers.models import CustomUser
 
 
 class index(View):
 
     def get(self, request):
+        if not request.user.is_authenticated:
+            return HttpResponseRedirect(reverse('login'))
+        else:
 
-        html = "index.html"
+            html = "index.html"
 
-        albums = Album.objects.all()
+            albums = Album.objects.all()
 
-        return render(request, html, {'albums': albums})
+            return render(request, html, {'albums': albums})
+
+
+class albumview(View):
+    def get(self, request, id):
+
+        html = "album_details.html"
+
+        album = Album.objects.filter(id=id)
+
+        return render(request, html, {'album': album})
+
+
+class userview(View):
+    def get(self, request, id):
+
+        html = "user_details.html"
+
+        user = CustomUser.objects.filter(id=id)
+
+        return render(request, html, {'user': user})
+
